229aa81041c92cc4578b15687de83b9b55641bcd
Thanawat Thangpiboonvech <thanawat.th@readyplanet.com>
2016-05-27 14:38:46 +0700

    add karma-jasmine

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/resources', express.static(__dirname + '/resources'));
 
 app.get('/', function (req, res) {
     res.sendFile('resources/html/index.html', { root: __dirname });
---
+++ resources/js/index.js
@@ -0,0 +1,4 @@
+angular.module('pipeApp',[])
+.controller('dashboardController', function($scope) {
+
+});
