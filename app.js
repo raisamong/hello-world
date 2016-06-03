6c88538bb7caeb2b54d2f274df1e57ab14efdc19
Thanawat Thangpiboonvech <thanawat.th@readyplanet.com>
2016-06-03 11:27:02 +0700

    add http

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/resources', express.static(__dirname + '/resources'));
 
 app.get('/', function (req, res) {
-    console.log(req);
-    console.log(res);
+    console.log(req.body);
+//    console.log(res);
     res.sendFile('resources/html/index.html', { root: __dirname });
 });
 
@@ -29,5 +29,9 @@ app.get('/about', function (req, res) {
 app.get('/index', function (req, res) {
     res.send('Hello World!');
 });
+
+//app.listen(4000, function () {
+//    console.log('Example app listening on port 4000!');
+//});
 
 module.exports = app;
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,27 @@
 angular.module('pipeApp',[])
-.controller('dashboardController', function($scope) {
+.controller('dashboardController', ['$scope', '$http', '$q',
+                                 function($scope, $http, $q) {
     $scope.test = "what the fuck";
-});
+    $scope.testHttp = function () {
+        JSON.stringify([1, 'false', false]);
+        console.log('test http');
+        $http({
+            method: 'GET',
+            url: 'http://10.1.106.67:4000/',
+            headers: {
+                "Content-type": "application/json;charset=UTF-8",
+                "access-control-allow-origin": " *"
+            },
+            data: JSON.stringify({
+                formId: '1',
+                start: '2',
+                limit: '3',
+            })
+        }).success(function (data, status, headers, config) {
+            console.log('success');
+        }).
+        error(function (data, status, headers, config) {
+            console.log('error', data);
+        });
+    };
+}]);
