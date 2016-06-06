2d152e336d1caf4654baa1f2fc631c7744482af3
Thanawat Thangpiboonvech <thanawat.th@readyplanet.com>
2016-06-06 11:25:59 +0700

    add file conf.js

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,16 +1,6 @@
 angular.module('loginController', [])
 .controller('LoginCtrl', ['$scope', '$http', '$q',
                         function($scope, $http, $q) {
-    String.prototype.hashCode = function() {
-        var hash = 0, i, chr, len;
-        if (this.length === 0) return hash;
-        for (i = 0, len = this.length; i < len; i++) {
-        chr   = this.charCodeAt(i);
-        hash  = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-    };
 
     $scope.login = function (usernameValid, passwordValid) {
         console.log('login');
