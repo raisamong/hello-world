41f57b2675a01db28ad52ce6682f8c742bff19d0
Thanawat Thangpiboonvech <thanawat.th@readyplanet.com>
2016-06-06 15:04:55 +0700

    create factory login

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,27 +1,50 @@
 angular.module('loginController', [])
-.controller('LoginCtrl', ['$scope', '$http', '$q',
-                        function($scope, $http, $q) {
+.controller('LoginCtrl', ['$scope', 'loginService',
+                        function($scope, loginService) {
+    var service = new loginService();
+    var genUserInfo = function () {
+        var info = {
+            username : $scope.username,
+            password : $scope.password
+        };
+        return info;
+    }
 
     $scope.login = function (usernameValid, passwordValid) {
         console.log('login');
         if (usernameValid, passwordValid) {
-            $http({
-               method: 'POST',
-               url: backend + 'login',
-               headers: {
-                   "Content-type": "application/json;charset=UTF-8",
-                   "access-control-allow-origin": " *"
-               },
-               data: JSON.stringify({
-                   username: $scope.username,
-                   password: $scope.password
-               })
-            }).success(function (data, status, headers, config) {
-               console.log('success', data);
-
-            }).error(function (data, status, headers, config) {
-               console.log('error', data);
+            service.login(genUserInfo()).then(function (res) {
+                console.log(res);
+            }, function (err) {
+                console.log(err);
             });
         }
     };
-}]);
+}])
+.factory('loginService', function ($http, $q) {
+    return function (){
+        var self = this;
+
+        this.login = function (info) {
+            var deferred = $q.defer();
+                $http({
+                   method: 'POST',
+                   url: backend + 'login',
+                   headers: {
+                       "Content-type": "application/json;charset=UTF-8",
+                       "access-control-allow-origin": " *"
+                   },
+                   data: JSON.stringify({
+                       username: info.username,
+                       password: info.password
+                   })
+                }).success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+
+                }).error(function (data, status, headers, config) {
+                    deferred.reject(data);
+                });
+            return deferred.promise;
+        };
+    }
+});
