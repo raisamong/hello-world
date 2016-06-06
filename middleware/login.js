a4687bba9ee096c45530851cefad8acaf7b34f16
Thanawat Thangpiboonvech <thanawat.th@readyplanet.com>
2016-06-06 17:00:20 +0700

    add middleware route

--- middleware/login.js
+++ middleware/login.js
@@ -4,8 +4,32 @@ var libAuth = require('../lib/auth.js')
 var sqlLogin =
 ['SElECT * FROM user WHERE email="','"AND password="'];
 
-router.use('/login', function (req, res) {
-    console.log('hash',libAuth.pwHash(req.body.password));
+router.route('/login')
+    .get(function(req, res) {
+        res.send('Get a random book');
+    })
+    .post(function(req, res) {
+        console.log('hash',libAuth.pwHash(req.body.password));
+        global.connection.query( sqlLogin[0] + req.body.username + sqlLogin[1]+ req.body.password +'"', function(err, rows, fields) {
+            if (rows.length) {
+                res.json({
+                    result: 0,
+                    data: rows
+                });
+            }
+            else{
+                res.json({
+                    result: 1,
+                    msg: 'data not exist'
+                });
+            }
+        });
+    })
+    .put(function(req, res) {
+        res.send('Update the book');
+    });
+
+router.use('/register', function (req, res) {
     global.connection.query( sqlLogin[0] + req.body.username + sqlLogin[1]+ req.body.password +'"', function(err, rows, fields) {
         if (rows.length) {
             res.json({
