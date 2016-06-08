2db343708e9199790ce0602ee4eec89e8d2a245e
Thanawat Thangpiboonvech <thanawat.th@readyplanet.com>
2016-06-08 16:45:41 +0700

    add register response

--- resources/js/register.js
+++ resources/js/register.js
@@ -2,6 +2,7 @@ angular.module('registerModule', [])
 .controller('RegisterCtrl', [ '$scope', 'registerService',
                             function ($scope, registerService) {
     var service = new registerService();
+
     var genUserInfo = function () {
         var info = {
             username : $scope.username,
@@ -11,12 +12,31 @@ angular.module('registerModule', [])
         return info;
     }
 
+    var genResult = function (output) {
+        console.log(output);
+        if (output.result == 0) {
+            // register success
+            $scope.registered = true;
+            $scope.regResult = 'Register Successfully';
+        }
+        else if (output.result == 1) {
+            //username exist
+            $scope.registerFailed = true;
+            $scope.regResult = 'Username already exist';
+        }
+        else if (output.result == 2) {
+            //register failed
+            $scope.registerFailed = true;
+            $scope.regResult = 'Register failed. Please try again'
+        }
+    };
+
+    $scope.registerStatus =;
+
     $scope.register = function () {
         console.log('register');
         service.register(genUserInfo()).then(function (res) {
-            console.log(res);
-        }, function (err) {
-            console.log(err);
+            genResult(res);
         });
     }
 }])
@@ -43,7 +63,7 @@ angular.module('registerModule', [])
                     deferred.resolve(data);
                 })
                 .error( function (data, status, headers, config) {
-                    deferred.reject(data);
+                    deferred.resolve(data);
                 });
             return deferred.promise;
         };
