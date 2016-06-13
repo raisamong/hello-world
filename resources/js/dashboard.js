b2a4ac733b7f1cd6594a717a32848385b004049a
Thanawat <thanawat.pipe.work@gmail.com>
2016-06-13 22:56:45 +0700

    add dashboard simple UI

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -1,11 +1,11 @@
 angular.module('dashboardModule', [])
 .controller('DashboardCtrl', [ '$scope', '$rootScope', '$state',
                                 function ($scope, $rootScope, $state) {
-    var checkCurrentUser = function () {
-        if (!$rootScope.profile) {
-            $state.go('login');
-        }
-    }
-
-    checkCurrentUser();
+//    var checkCurrentUser = function () {
+//        if (!$rootScope.profile) {
+//            $state.go('login');
+//        }
+//    }
+//
+//    checkCurrentUser();
 }]);
