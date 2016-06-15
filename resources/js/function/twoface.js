ecbb8e5ff1ae93ade3a8612b2769733a2164f4bc
Thanawat <thanawat.pipe.work@gmail.com>
2016-06-15 21:59:26 +0700

    add date time picker

--- resources/js/function/twoface.js
+++ resources/js/function/twoface.js
@@ -1,5 +1,7 @@
 angular.module('twofaceModule', [])
 .controller('TwofaceCtrl', [ '$scope',
                                 function () {
+     $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+      $scope.format = $scope.formats[0];
 
 }]);
