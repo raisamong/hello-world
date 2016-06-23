4bd9caf6b53f4c718fb5f816cb6362c5c5a61851
Thanawat <thanawat.pipe.work@gmail.com>
2016-06-23 23:15:06 +0700

    add twoface add contact modal

--- resources/js/function/twoface.js
+++ resources/js/function/twoface.js
@@ -1,8 +1,45 @@
 angular.module('twofaceModule', [])
-.controller('TwofaceCtrl', ['$scope',
-                            function ($scope) {
+.controller('TwofaceCtrl', ['$scope', '$uibModal',
+                            function ($scope, $uibModal) {
     $scope.info = {};
     $scope.test = function () {
         console.log('eeee', $scope.info.date);
-    }
+    };
+    $scope.info.contacts = [];
+    $scope.addContact = function () {
+        var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: '../resources/template/modal/twoface.add.contact.html',
+            animation: true,
+            controller: modalAddContactCtrl,
+            size: 'md',
+            resolve: {
+
+            }
+        });
+
+        modalInstance.result.then(function (contact) {
+            $scope.info.contacts.push(contact);
+            console.log($scope.info);
+        }, function (dismiss) {
+
+        });
+
+        function modalAddContactCtrl($scope, $uibModalInstance){
+            $scope.ok = function () {
+                var info = {
+                    name: $scope.name,
+                    position: $scope.position,
+                    tel: $scope.tel,
+                    email: $scope.email
+                };
+                console.log(info);
+                $uibModalInstance.close(info);
+            };
+
+            $scope.cancel = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
+        }
+    };
 }]);
